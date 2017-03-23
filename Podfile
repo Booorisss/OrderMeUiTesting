4b7dc8d6ac0dc49c0e7f25cff519f8fd36534a7b
Boris <borisgurtovoyy@gmail.com>
2017-03-22 20:40:31 -0700

    initial commit

---
+++ Podfile
@@ -0,0 +1,20 @@
+source 'https://github.com/CocoaPods/Specs.git'
+platform :ios, ‘9.0’
+
+use_frameworks!
+
+target “orderMe” do
+pod ‘Alamofire’, '~> 4.0’
+pod 'ObjectMapper', '~> 2.0’
+pod 'AlamofireImage', '~> 3.1’
+pod 'FacebookCore'
+pod 'FacebookLogin'
+pod 'FacebookShare'
+end
+
+target “OrderMEUITests” do
+pod ‘Alamofire’, '~> 4.0’
+pod 'Alamofire-Synchronous', '~> 4.0'
+pod 'ObjectMapper', '~> 2.0’
+pod 'AlamofireImage', '~> 3.1’
+end
---
+++ orderMe/SimulatorTableId.swift
@@ -0,0 +1,35 @@
+//
+//  SimulatorTableId.swift
+//  orderMe
+//
+//  Created by Boris Gurtovyy on 13.04.16.
+//  Copyright © 2016 Boris Gurtovoy. All rights reserved.
+//
+
+import UIKit
+
+
+// due to unability to use camera for QR_CODE in simulator
+class SimulatorTableId: UIViewController {
+
+    @IBOutlet weak var tid: UITextField!
+    
+    override func viewDidLoad() {
+        navigationController?.isNavigationBarHidden = false
+        self.tid.accessibilityIdentifier = "@table_number_textfield"
+    }
+
+    
+    
+    
+    @IBAction func button(_ sender: AnyObject) {
+        if let myTableId = Int(tid.text!){
+            let sTone = SingleTone.shareInstance
+            sTone.tableID = myTableId
+            _ = self.navigationController?.popViewController(animated: true)
+        }
+        
+    }
+    
+
+}
